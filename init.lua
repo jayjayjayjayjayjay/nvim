0b9f255e0b5713f26b2469a87e0879bf0b22b757
jay <u6044681@anu.edu.au>
2025-01-05 00:49:16 +1100

    Get conform working with c files

--- init.lua
+++ init.lua
@@ -45,6 +45,8 @@ lspconfig.volar.setup {
   },
 }
 
+lspconfig.html.setup{}
+
 ---linting
 require("conform").setup({
   formatters_by_ft = {
@@ -54,7 +56,35 @@ require("conform").setup({
     -- Use a sub-list to run only the first available formatter
     javascript = { { "prettierd", "prettier" } },
     typescript = { { "prettierd", "prettier" } },
+    html = { { "prettierd", "prettier" } },
+    c = {  "clang-format" },
   },
+})
+require("conform").setup({
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_format = "fallback" }
+  end,
+})
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
 })
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
